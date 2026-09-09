97b0d4bed8829a1811daf555f1b52f3d4f774ffc
Javi <javiera.lizana.ch@gmail.com>
2026-09-09 17:12:00 -0300

    strassem actualizado creo

--- T1/code/matrix_multiplication/algorithms/strassen.cpp
+++ T1/code/matrix_multiplication/algorithms/strassen.cpp
@@ -8,90 +8,88 @@
 
 using namespace std;
 
-//funcion aux para sumar 2 matrices
-vector<vector<int>> sumar(const vector<vector<int>>& A, const vector<vector<int>>& B){
-    int n = A.size();
-    vector<vector<int>> C(n, vector<int>(n,0));
-    for(int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            C[i][j] = A[i][j] + B[i][j];
-        }
+//cambio para optimizacion 2:
+//Representacion plana de una matriz n x n: acceso mediante M[i*n + j]
+using Matriz = vector<int>;
+
+//funcion aux para sumar 2 matrices (representacion plana)
+Matriz sumar(const Matriz& A, const Matriz& B, int n){
+    Matriz C(n * n);
+
+    for(int i = 0; i < n * n; n++){
+        C[i] = A[i] + B[i];
     }
     return C;
 }
 
-//funcion aux para restar 2 matrices
-vector<vector<int>> restar(const vector<vector<int>>& A, const vector<vector<int>>& B){
-    int n = A.size();
-    vector<vector<int>> C(n, vector<int>(n,0));
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            C[i][j] = A[i][j] - B[i][j];
-        }
+//funcion aux para restar 2 matrices (representacion plana)
+Matriz restar(const Matriz& A, const Matriz& B, int n){
+    Matriz C(n * n);
+
+    for(int i = 0; i < n * n; n++){
+        C[i] = A[i] - B[i];
     }
+
     return C;
 }
 
-//algoritmo de strassen
-vector<vector<int>> Strassen(const vector<vector<int>>& M1, const vector<vector<int>>& M2){
-    int n = M1.size();
+//extrae una submatriz de tamaño mitad x mitad desde la esquina (filaOffset, colOffset) de M (de tamaño n x n)
+Matriz extraerSubM(const Matriz& M, int n, int mitad, int filaOffset, int colaOffset){
+    Matriz Sub(mitad * mitad);
+    for(int i = 0; i < mitad; i++){
+        for(int j = 0; j < mitad; j++){
+            Sub[i * mitad + j] = M[(i + filaOffset) * n + (j + colaOffset)];
+        }
+    }
+    return Sub;
+}
+
+//algoritmo de strassen (representacion plana), recibe y retorna matrices de tamaño n x n
+Matriz Strassen(const Matriz& M1, const Matriz& M2, int n){
 
     //caso base: si la matriz es 1x1, se retorna el producto de sus unicos elementos
     if (n == 1){
-        return {{M1[0][0] * M2[0][0]}};
+        return {{M1[0] * M2[0]}};
     }
 
     int mitad = n/2;
 
-    //inicializacion de las submatrices n/2 x n/2
-    vector<vector<int>> A(mitad, vector<int>(mitad));
-    vector<vector<int>> B(mitad, vector<int>(mitad));
-    vector<vector<int>> C(mitad, vector<int>(mitad));
-    vector<vector<int>> D(mitad, vector<int>(mitad));
-    vector<vector<int>> E(mitad, vector<int>(mitad));
-    vector<vector<int>> F(mitad, vector<int>(mitad));
-    vector<vector<int>> G(mitad, vector<int>(mitad));
-    vector<vector<int>> H(mitad, vector<int>(mitad));
+    //extraemos las 4 submatrices de M1 y M2 (A,B,C,D y E,F,G,H respectivamente)
+    Matriz A = extraerSubM(M1, n, mitad, 0, 0);
+    Matriz B = extraerSubM(M1, n, mitad, 0, mitad);
+    Matriz C = extraerSubM(M1, n, mitad, mitad, 0);
+    Matriz D = extraerSubM(M1, n, mitad, mitad, mitad);
 
-    //dividimos las matrices M1 y M2 en 4 submatrices c/u (de las que ya generamos)
-    for(int i = 0; i < mitad; i++){
-        for(int j = 0; j < mitad; j++){
-            A[i][j] = M1[i][j];
-            B[i][j] = M1[i][j + mitad];
-            C[i][j] = M1[i + mitad][j];
-            D[i][j] = M1[i + mitad][j + mitad];
-
-            E[i][j] = M2[i][j];
-            F[i][j] = M2[i][j + mitad];
-            G[i][j] = M2[i + mitad][j];
-            H[i][j] = M2[i + mitad][j + mitad];
-        }
-    }
+    Matriz E = extraerSubM(M2, n, mitad, 0, 0);
+    Matriz F = extraerSubM(M2, n, mitad, 0, mitad);
+    Matriz G = extraerSubM(M2, n, mitad, mitad, 0);
+    Matriz H = extraerSubM(M2, n, mitad, mitad, mitad);
 
     //Calculamos los 7 productos P1 a P7 recursivamente siguiendo las ec de strassen
-    vector<vector<int>> P1 = Strassen(A, restar(F,H));
-    vector<vector<int>> P2 = Strassen(sumar(A,B), H);
-    vector<vector<int>> P3 = Strassen(sumar(C,D), E);
-    vector<vector<int>> P4 = Strassen(D, restar(G, E));
-    vector<vector<int>> P5 = Strassen(sumar(A,D), sumar(E,H));
-    vector<vector<int>> P6 = Strassen(restar(B, D), sumar(G,H));
-    vector<vector<int>> P7 = Strassen(restar(A,C), sumar(E,F));
+    Matriz P1 = Strassen(A, restar(F, H, mitad), mitad);
+    Matriz P2 = Strassen(sumar(A, B, mitad), H, mitad);
+    Matriz P3 = Strassen(sumar(C, D, mitad), E, mitad);
+    Matriz P4 = Strassen(D, restar(G, E, mitad), mitad);
+    Matriz P5 = Strassen(sumar(A, D, mitad), sumar(E, H, mitad), mitad);
+    Matriz P6 = Strassen(restar(B, D, mitad), sumar(G, H, mitad), mitad);
+    Matriz P7 = Strassen(restar(A, C, mitad), sumar(E, F, mitad), mitad);
 
     //combinamos las submatrices resultantes en los cuadrantes finales
-    vector<vector<int>> C11 = sumar(restar(sumar(P5, P4), P2), P6);
-    vector<vector<int>> C12 = sumar(P1, P2);
-    vector<vector<int>> C21 = sumar(P3, P4);
-    vector<vector<int>> C22 = restar(restar(sumar(P1, P5), P3), P7);
-    
-    //armamos la matriz final resultante de tamaño n x n
-    vector<vector<int>> Resultado(n, vector<int>(n));
-    for(int i = 0; i < mitad; i++){
+    Matriz C11 = sumar(restar(sumar(P5, P4, mitad), P2, mitad), P6, mitad);
+    Matriz C12 = sumar(P1, P2, mitad);
+    Matriz C21 = sumar(P3, P4, mitad);
+    Matriz C22 = restar(restar(sumar(P1, P5, mitad), P3, mitad), P7, mitad);
+
+    //armamos la matriz final resultante de tamaño n x n directamente sobre el arreglo plano
+    Matriz Resultado(n * n);
+    for (int i = 0; i < mitad; i ++){
         for(int j = 0; j < mitad; j++){
-            Resultado[i][j] = C11[i][j];
-            Resultado[i][j + mitad] = C12[i][j];
-            Resultado[i + mitad][j] = C21[i][j];
-            Resultado[i + mitad][j + mitad] = C22[i][j];
+            Resultado[i * n + j] = C11[i * mitad + j];
+            Resultado[i * n + (j + mitad)] = C12[i * mitad + j];
+            Resultado[(i + mitad) * n + j] = C21[i * mitad + j];
+            Resultado[(i + mitad) * n + (j + mitad)] = C22[i * mitad + j];
         }
     }
+    
     return Resultado;
 }
